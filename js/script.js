c122694c5e15fb36007eb9575b17106cd268d5b8
Julio leon <julioleon@Julios-MBP.localdomain>
2023-02-10 10:20:14 -0500

    finished winning logic and addded reset button

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 /*----- constants -----*/
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
 
 /*----- app's state (variables) -----*/
 let stateBoard
@@ -11,6 +21,7 @@ const messages = document.querySelector('h2')
 
 /*----- event listeners -----*/
 document.querySelector('#board').addEventListener('click', handleTurn)
+document.querySelector('#reset-button').addEventListener('click', init)
 
 /*----- functions -----*/
 
@@ -28,20 +39,27 @@ function render() {
     board.forEach((mark, index) => {
         squares[index].textContent = mark
     })
-    messages.textContent = `It's ${stateTurn}'s turn!`
+    messages.textContent = win === 'T' ? "That's a tie!" : win ? `${win} wins the game!` : `It's ${stateTurn}'s turn!`
 }
 
 function handleTurn(e) {
     board[squares.findIndex(square => square === e.target)] = stateTurn
     stateTurn = stateTurn === 'X' ? 'O' : 'X'
 
-    win = board[0] && board[0] === board[1] && board[0] === board[2] ? board[0] : null
+    win = getWinner()
 
+    render()
     console.log(board)
 }
 
 function getWinner() {
-    
+    let winner = null
+    winningCombinations.forEach((combination, index) => {
+        if (board[combination[0]] && board[combination[0]] === board[combination[1]] && board[combination[0]] === board[combination[2]]) {
+            winner = board[combination[0]]
+        }
+    })
+    return winner ? winner : board.includes('') ? null : 'T'
 }
 
 init()
