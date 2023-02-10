4042b107c7e67cf1723f8d5c17056b76e56766ac
Julio leon <julioleon@Julios-MBP.localdomain>
2023-02-10 10:04:57 -0500

    added turn and clicl logic

--- js/script.js
+++ js/script.js
@@ -2,10 +2,15 @@
 
 /*----- app's state (variables) -----*/
 let stateBoard
+let stateTurn = 'X'
+let win
 
 /*----- cahed element references -----*/
+const squares = Array.from(document.querySelectorAll('#board div'))
+const messages = document.querySelector('h2')
 
 /*----- event listeners -----*/
+document.querySelector('#board').addEventListener('click', handleTurn)
 
 /*----- functions -----*/
 
@@ -15,6 +20,24 @@ function init() {
         '', '', '',
         '', '', ''
     ]
+
+    render()
+}
+
+function render() {
+    board.forEach((mark, index) => {
+        squares[index].textContent = mark
+    })
+    messages.textContent = `It's ${stateTurn}'s turn!`
+}
+
+function handleTurn(e) {
+    board[squares.findIndex(square => square === e.target)] = stateTurn
+    stateTurn = stateTurn === 'X' ? 'O' : 'X'
+
+    win = board[0] && board[0] === board[1] && board[0] === board[2] ? board[0] : null
+
+    console.log(board)
 }
 
 init()
